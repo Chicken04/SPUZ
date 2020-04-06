4fc1e869d893958ebe353eea6474ac1969598ae5
Jerry Li <34424837+Chicken04@users.noreply.github.com>
2020-04-06 18:59:50 -0400

    Added range

--- Python Code/SPUZ.py
+++ Python Code/SPUZ.py
@@ -1,24 +1,24 @@
 import random
 
-q = int(input("Enter vector size: "))
-r = int(input("Enter lower interation : "))
-s = int(input("Enter upper interation : "))
+vec_size = int(input("Enter vector size: "))
+lower = int(input("Enter lower interation : "))
+upper = int(input("Enter upper interation : "))
+rang = int(input("Enter range of values: "))
 l = []
 filenames = []
-for u in range (r,s+1):
+for u in range (lower,upper+1):
   filenames.append(str(u)+".txt")
 print(filenames)
 
-def spuz(lower, upper, size):
+def spuz(iteration, size, rang):
 
-  for i in range (0, upper):  # Start of For Loop
-    print(i)
+  for i in range (0, size):  # Start of For Loop
     n = 0
     null = False
     a = []
 
     for i in range(0, size):  # Start of For Loop
-      a.append(random.randint(0, 10))
+      a.append(random.randint(0, rang))
       # End of For Loop
 
     f = 0
@@ -47,10 +47,9 @@ def spuz(lower, upper, size):
     # End of For Loop
   return(l)
 
-
 for filename in filenames:
     with open(filename, "w") as output_file:
       o = int(filename.replace(".txt",""))
-      result = spuz(r, s, q)
+      result = spuz(o, vec_size, rang)
       for i in range(0,len(result)):
-        output_file.write(str(result[i]) + "\n")
+        output_file.write(str(result[i]) + " ")
