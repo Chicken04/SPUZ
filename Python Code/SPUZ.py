381eefd6a3f1d4030621ac4a9cbe9e05158cb9ae
Jerry Li <34424837+Chicken04@users.noreply.github.com>
2020-04-06 18:43:25 -0400

    Some message

--- Python Code/SPUZ.py
+++ Python Code/SPUZ.py
@@ -1,48 +1,56 @@
 import random
 
 q = int(input("Enter vector size: "))
-r = int(input("Enter number of sims: "))
+r = int(input("Enter lower interation : "))
+s = int(input("Enter upper interation : "))
 l = []
+filenames = []
+for u in range (r,s+1):
+  filenames.append(str(u)+".txt")
+print(filenames)
 
-for i in range (0, r + 1):  # Start of For Loop
+def spuz(lower, upper, size):
 
-  n = 0
-  null = False
-  a = []
+  for i in range (0, upper):  # Start of For Loop
+    print(i)
+    n = 0
+    null = False
+    a = []
 
-  for i in range(0, q):  # Start of For Loop
-    a.append(random.randint(0, 10))
+    for i in range(0, size):  # Start of For Loop
+      a.append(random.randint(0, 10))
+      # End of For Loop
+
+    f = 0
+
+    while null != True:  # Start of While Loop
+
+      m=0
+
+      for j in range (0, len(a)):  # Start of For Loop
+        a[j] = random.randint(0, 10) * a[j]
+      # End of For Loop
+
+      for k in range (0, len(a)):  # Start of For Loop
+        if a[k] == 0:  # Start of If Loop
+          m = m + 1
+          # End of If Loop
+
+      if m == 10:  # Start of If Loop
+        null = True
+      # End of If Loop
+
+      f = f + 1
+      # End of While Loop
+
+    l.append(f)
     # End of For Loop
+  return(l)
 
-  f = 0
 
-  while null != True:  # Start of While Loop
-
-    m=0
-
-    for j in range (0, len(a)):  # Start of For Loop
-      a[j] = random.randint(0, 10) * a[j]
-    # End of For Loop
-
-    for k in range (0, len(a)):  # Start of For Loop
-      if a[k] == 0:  # Start of If Loop
-        m = m + 1
-        # End of If Loop
-
-    if m == 10:  # Start of If Loop
-      null = True
-    # End of If Loop
-
-    f = f + 1
-    # End of While Loop
-
-  l.append(f)
-# End of For Loop
-
-print(l)
-print(min(l))
-print(max(l))
-
-with open("data.txt", "w") as output_file:
-  for i in range(0,len(l)):
-    output_file.write(str(l[i]) + "\n")
+for filename in filenames:
+    with open(filename, "w") as output_file:
+      o = int(filename.replace(".txt",""))
+      result = spuz(r, s, q)
+      for i in range(0,len(result)):
+        output_file.write(str(result[i]) + "\n")
